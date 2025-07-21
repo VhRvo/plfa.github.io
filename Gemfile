d1081b8b833b2557c3e5873b9600be157752e02b
VhRvo <zyzhuo@outlook.com>
2025-07-21 15:55:34 +0800

    Delete everything but source code.

--- Gemfile
+++
@@ -1,5 +0,0 @@
-# Used by `make test-htmlproofer`
-
-source 'https://rubygems.org'
-
-gem 'html-proofer', "~> 5.0.3", :require => false
--- web/assets/js/main.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener('DOMContentLoaded', function(event) {
-  anchors.add('h1').add('h2').add('h3').add('h4');
-  darkmode('stylesheet-light', 'stylesheet-dark');
-});
